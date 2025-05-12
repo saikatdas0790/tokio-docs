0e3ba7787061dcb4757b86b54fdc0a0267993292
Saikat Das <saikatdas0790@gmail.com>
2025-05-12 18:16:34 +0000

    Refactor log message for clarity in Redis client interaction

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -5,9 +5,7 @@ async fn main() -> Result<()> {
     let mut client = client::connect("127.0.0.1:6379").await?;
     client.set("hello", "world".into()).await?;
     let result = client.get("hello").await?;
-    println!("Got a result from Redis: {result:?}");
+    println!("got value from the server; result={:?}", result);
 
     Ok(())
 }
-
-// task runner instead of every time running cli commands
