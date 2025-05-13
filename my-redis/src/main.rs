c33c05587c6d58cbbc0c85a504849c21eae81ccd
Saikat Das <saikatdas0790@gmail.com>
2025-05-13 13:53:27 +0000

    Refactor main and process functions for improved readability; enhance command handling in Redis server

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,4 +1,4 @@
-use mini_redis::{Connection, Frame};
+use mini_redis::{Connection, Frame, frame};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -7,17 +7,35 @@ async fn main() {
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    let mut db = HashMap::new();
+
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
-
-        let response = Frame::Error("unimplemented".to_string());
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented: {cmd:?}"),
+        };
         connection.write_frame(&response).await.unwrap();
     }
 }
